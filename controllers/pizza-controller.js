1393b3a8b1180d640efa74655e979c84a2f3c5ba
sakme <alan@akme.us>
2022-03-06 16:01:38 -0500

    added validators

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -43,15 +43,15 @@ const pizzaController = {
     },
 
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new:true })
-        .then(dbPizzaData => {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+          .then(dbPizzaData => {
             if (!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id!' });
-                return;
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
             }
             res.json(dbPizzaData);
-        })
-        .catch(err => res.status(400).json(err));
+          })
+          .catch(err => res.status(400).json(err));
     },
 
     deletePizza({ params }, res) {
