832ef719a99e4daa9e43bc709ca2bd81a6d0b397
sakme <alan@akme.us>
2022-03-06 14:51:40 -0500

    created reply schema and routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
@@ -14,6 +15,22 @@ const commentController = {
         .then(dbPizzaData => {
             if (!dbPizzaData) {
                 res.status(404).json({ message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
                 return;
             }
             res.json(dbPizzaData);
@@ -40,6 +57,16 @@ const commentController = {
             }
             res.json(dbPizzaData);
         })
+        .catch(err => res.json(err));
+    },
+
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.json(err));
     }
 };
