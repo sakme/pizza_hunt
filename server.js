c5ccdc2bf270b3509be61fd95409fed4e7fdac5e
sakme <alan@akme.us>
2022-03-06 16:28:16 -0500

    db connection

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
